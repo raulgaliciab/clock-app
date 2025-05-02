657d3e9a4cd59d0c81f5d6958c4add3707d810b2
Raul Galicia <raul.galiciab@gmail.com>
2025-05-01 22:47:52 -0600

    Images path

--- src/components/ButtonExpansion.tsx
+++ src/components/ButtonExpansion.tsx
@@ -2,7 +2,7 @@ export const ButtonExpansion = () => {
   return (
     <button>
       LESS
-      <img src="/public/assets/desktop/icon-arrow-down.svg" alt="Expand button" />
+      <img src="/assets/desktop/icon-arrow-down.svg" alt="Expand button" />
     </button>
   )
 }
--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
+import { getTimeAPI } from '../utils/getTime';
+
 export const Time = () => {
+
+  useEffect(() => {
+    getTimeAPI().then( data => {
+      console.log(data);
+    })
+    .catch( err => {
+      console.error(err)
+    })
+  });
+
   return (
     <section>
       <span>
-        <img src="/public/assets/desktop/icon-sun.svg" alt="Sun Icon" />
+        <img src="/assets/desktop/icon-sun.svg" alt="Sun Icon" />
         GOOD MORNING, IT'S CURRENTLY
       </span>
       <p>11:37 <span>BST</span></p>
