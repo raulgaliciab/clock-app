7625a867488e27eb7eee6ad18c9ad7268d42990e
Raul Galicia <raul.galiciab@gmail.com>
2025-05-01 18:22:56 -0600

    getTime async function and TimeApiResponse TS type

---
+++ src/types/TimeApiResponse.ts
@@ -0,0 +1,7 @@
+export type TimeApiResponse = {
+  "day_of_week": number,
+  "day_of_year": number,
+  "datetime": string,
+  "week_number": number,
+  "abbreviation": string
+}
---
+++ src/utils/getTime.ts
@@ -0,0 +1,21 @@
+import { TimeApiResponse } from '../types/TimeApiResponse';
+
+export const getTimeAPI = async () :Promise<TimeApiResponse> => {
+  const API_URL:string = 'http://worldtimeapi.org/api/ip';
+
+    try {
+      const apiRes:Response = await fetch(API_URL);
+
+      if (!apiRes.ok) {
+        throw new Error(`Error HTTP: ${apiRes.status}`);
+      }
+
+      const data:TimeApiResponse = await apiRes.json();
+      return data;
+    }
+
+    catch (err) {
+      console.error("Error type:", err);
+      throw err;
+    }
+}
