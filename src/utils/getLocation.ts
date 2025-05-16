36ed0c9bc4144f982dc1cdb0ec06fd82266be3fc
Raul Galicia <raul.galiciab@gmail.com>
2025-05-15 18:24:16 -0600

    New API for location. Fix on getTime.ts

--- src/utils/getLocation.ts
+++ src/utils/getLocation.ts
@@ -2,28 +2,23 @@ import { LocationApiResponse } from '../types/LocationApiResponse';
 
 export const getLocation = async ():Promise<LocationApiResponse> => {
   const API_URL:string = import.meta.env.VITE_LOCATION_URL;
-  const API_KEY:string = import.meta.env.VITE_LOCATION_API_KEY;
 
   try {
-    const apiRes:Response = await fetch(API_URL, {
-      headers: {
-        "apikey": API_KEY
-      }
-    })
+    const apiRes:Response = await fetch(API_URL);
 
     if (!apiRes.ok) {
       throw new Error(`Error fetching location data: ${apiRes.status}`)
     }
 
-    const { data } = await apiRes.json();
+    const data = await apiRes.json();
 
-    if (!data?.location?.country?.alpha2 || !data?.location?.city?.name) {
+    if (!data?.country_code || !data?.city) {
       throw new Error("Incomplete location data received from API");
     }
 
     return {
-      "country": data.location.country.alpha2,
-      "city": data.location.city.name
+      "country": data.country_code,
+      "city": data.city
     }
   }
   catch (err) {
--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -1,7 +1,7 @@
 import { TimeApiResponse } from '../types/TimeApiResponse';
 
 export const getTimeAPI = async () :Promise<TimeApiResponse> => {
-  const API_URL:string = 'https://worldtimeapi.org/api/ip';
+  const API_URL:string = import.meta.env.VITE_TIME_URL;
 
     try {
       const apiRes:Response = await fetch(API_URL);
